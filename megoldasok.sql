b9aff15cde157004ffd1668213e8313b6e8fad4d
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-03-24 12:58:40 +0100

    asd
    asd

---
+++ megoldasok.sql
@@ -0,0 +1,13 @@
+﻿--CREATE DATABASE ostermelo; 
+--USE ostermelo;
+
+--SELECT DISTINCT telepules FROM partnerek
+--ORDER BY telepules;
+
+--SELECT COUNT(sorsz) AS alkalmak
+--FROM kiszallitasok 
+--INNER JOIN partnerek
+--ON partnerek.id = partnerid
+--WHERE telepules LIKE 'Vác';
+
+
